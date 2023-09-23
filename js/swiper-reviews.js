9d30b440732cd21dd7ee72d492cbd753390532a2
turov <ilya.turov@gmail.com>
2023-09-23 15:26:19 +0300

    кыаыв

--- js/swiper-reviews.js
+++ js/swiper-reviews.js
@@ -25,3 +25,24 @@ if (x.matches) {
         },
     });
 }
+
+const swiper2 = new Swiper('.swiperVideo', {
+
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 3000,
+    },
+    breakpoints: {
+        1000: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+        },
+        1300: {
+            slidesPerView: 5,
+            spaceBetween: 40
+        }
+    }
+});
--- js/swiper.js
+++ js/swiper.js
@@ -8,3 +8,20 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+const swiper2 = new Swiper('.swiperVideo', {
+
+    direction: 'horizontal',
+    loop: true,
+    // slidesPerView: 4,
+    // spaceBetween: 10,
+    autoplay: {
+        delay: 100,
+    },
+    // breakpoints: {
+    //     640: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 40
+    //     }
+    // }
+});
