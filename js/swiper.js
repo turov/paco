278be2b3c2a2ec1e3289578ee2460900c8f24897
turov <ilya.turov@gmail.com>
2023-12-22 12:25:50 +0300

    update video slider

--- js/swiper.js
+++ js/swiper.js
@@ -15,20 +15,27 @@ if (document.querySelector('.swiperVideo')) {
     const swiper2 = new Swiper('.swiperVideo', {
 
         direction: 'horizontal',
-        loop: false,
+        loop: true,
         slidesPerView: 2,
         spaceBetween: 10,
         autoplay: {
             delay: 3000,
+            enabled: false,
         },
         breakpoints: {
             1000: {
                 slidesPerView: 5,
                 spaceBetween: 10,
+                autoplay: {
+                    enabled: true
+                },
             },
             1300: {
                 slidesPerView: 5,
-                spaceBetween: 40
+                spaceBetween: 40,
+                autoplay: {
+                    enabled: true
+                },
             }
         }
     });
